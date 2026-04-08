b8eb49ca72cbffa84cf201918f034dbfac811999
Ilian Verlee <ilian.verlee@gmail.com>
2026-04-08 16:09:16 +0200

    feat: finished the world map

--- js/charts/world_chart.js
+++ js/charts/world_chart.js
@@ -1,6 +1,7 @@
 let dataByYear = {};
 let tsvDataGlobal = [];
 let idToName = {};
+let currentYear = 0;
 
 
 
@@ -35,9 +36,14 @@ function renderWorldChart(){
 
 
 
+
+
     const pathGenerator = d3.geoPath().projection(projection);
 
 
+
+    const colorScale = d3.scaleSequential(d3.interpolateViridis)
+        .domain([0, 10]);
 
     const zoom = d3.zoom()
         .scaleExtent([1, 8])
@@ -90,31 +96,105 @@ function renderWorldChart(){
                 .translate(translate[0], translate[1])
                 .scale(scale)
         );
-        g.selectAll(".country").transition().style("fill", "#2c3e50");
-        d3.select(this).transition().style("fill", "red");
+        g.selectAll(".country").transition().attr("fill", "#2c3e50");
+        d3.select(this).transition().attr("fill", "red");
+
+
+        g.selectAll(".map-popup").remove();
+
+        const [[p0, s0], [p1, s1]] = pathGenerator.bounds(d);
+        const [p, s] = pathGenerator.centroid(geometry);
+        const popupGroup = g.append("g")
+            .datum(d)
+            .attr("class", "map-popup")
+            .attr("transform", `translate(${p}, ${s}) scale(${1/scale})`);
+
+        popupGroup.append("rect")
+            .attr("x", -75)
+            .attr("y", -50)
+            .attr("width", 150)
+            .attr("height", 60)
+            .attr("fill", "white")
+            .attr("stroke", "#333")
+            .attr("stroke-width", 2)
+            .attr("rx", 10);
+
+        popupGroup.append("text")
+            .attr("text-anchor", "middle")
+            .attr("y", -30)
+            .style("font-size", "14px")
+            .style("font-family", "sans-serif")
+            .text(idToName[d.id]);
+
+        popupGroup.append("text")
+            .attr("text-anchor", "middle")
+            .attr("y", -10)
+            .style("font-size", "12px")
+            .text(`Score: ${dataByYear[currentYear]?.[d.id] || "No Data"}`);
+
+        const closeBtnGroup = popupGroup.append("g")
+            .attr("transform", "translate(62, -40)")
+            .style("cursor", "pointer")
+            .on("click", (e) => {
+                e.stopPropagation();
+
+                popupGroup.remove();
+                reset();
+            });
+        closeBtnGroup.append("rect")
+            .attr("x", -10)
+            .attr("y", -10)
+            .attr("width", 20)
+            .attr("height", 20)
+            .attr("fill", "transparent")
+            .style("pointer-events", "all");
+
+        const xStrokeWidth = 2;
+
+        closeBtnGroup.append("line")
+            .attr("x1", -6).attr("y1", -6)
+            .attr("x2", 6).attr("y2", 6)
+            .attr("stroke", "#e74c3c")
+            .attr("stroke-width", xStrokeWidth)
+            .attr("stroke-linecap", "round");
+
+        closeBtnGroup.append("line")
+            .attr("x1", -6).attr("y1", 6)
+            .attr("x2", 6).attr("y2", -6)
+            .attr("stroke", "#e74c3c")
+            .attr("stroke-width", xStrokeWidth)
+            .attr("stroke-linecap", "round");
     }
 
 
-    d3.select("#reset_button").on("click", () => {
-        g.selectAll(".country").transition().style("fill", "#2c3e50");
+
+    function reset(){
+        g.selectAll(".map-popup").remove();
+        g.selectAll(".country").transition().attr("fill", d => {
+            const score = dataByYear[currentYear][d.id];
+
+            return score ? colorScale(+score) : "#ffffff";
+        });
 
         svg_base.transition()
             .duration(750)
             .call(zoom.transform, d3.zoomIdentity);
-    });
+    }
 
-
-    const colorScale = d3.scaleSequential(d3.interpolateBlues)
-        .domain([0, 10]);
 
     const nameAliases = {
-        "united states": "united states of america",
-        "us": "united states of america",
+        "czechia": "czech rep.",
         "south korea": "korea",
-        "Côte d'Ivoire": "Côte d'Ivoire",
+        "côte d’ivoire": "côte d'ivoire",
         "central african republic": "central african rep.",
         "congo (brazzaville)": "congo",
-        "DR Congo": "dem. rep. congo"
+        "dr congo": "dem. rep. congo",
+        "bosnia and herzegovina": "bosnia and herz.",
+        "dominican republic": "dominican rep.",
+        "north macedonia": "macedonia",
+        "south sudan": "s. sudan",
+        "laos": "lao pdr",
+        "north cyprus": "n. cyprus"
     };
 
     Promise.all([
@@ -128,6 +208,7 @@ function renderWorldChart(){
         const years = csvData.map(d => +d["Year"]).filter(d => !isNaN(d));
         const minYear = 2019;
         const maxYear = d3.max(years);
+        currentYear = maxYear
 
         const slider = d3.select("#year_slider")
             .attr("min", minYear)
@@ -137,7 +218,7 @@ function renderWorldChart(){
 
         d3.select("#year_label").text(maxYear);
 
-        updateMap(maxYear);
+        updateMap(currentYear);
 
         const nameToId = {};
         tsvData.forEach(d => {
@@ -145,24 +226,26 @@ function renderWorldChart(){
             idToName[d.iso_n3] = d.name;
         });
 
+
+
+
         csvData.forEach(d => {
             let rawName = d["Country name"];
-            if (!rawName) return;
-            if (rawName.includes("Ivoire")) {
-                console.log("Found Ivory Coast in CSV! Target ID:", nameToId["Côte d'Ivoire"]);
-            }
-            let name = rawName.toLowerCase().trim();
+            let year = d["Year"];
+            if (rawName) {
+                let cleanName = rawName.toLowerCase().trim();
 
-            if (/cote d'ivoire/i.test(name) || /ivory coast/i.test(name)) {
-                name = "cote d'ivoire";
-            }
+                if (nameAliases[cleanName]) {
+                    cleanName = nameAliases[cleanName];
+                }
 
-            const year = d["Year"];
-            const id = nameToId[ name];
+                const id = nameToId[cleanName];
 
-            if (id) {
-                if (!dataByYear[year]) dataByYear[year] = {};
-                dataByYear[year][id] = +d["Life evaluation (3-year average)"];
+                if (id) {
+                    if (!dataByYear[year]) dataByYear[year] = {};
+                    dataByYear[year][id] = +d["Life evaluation (3-year average)"];
+
+                }
             }
         });
 
@@ -189,24 +272,37 @@ function renderWorldChart(){
 
     }).catch(err => console.error("Error loading data:", err));
 
+
+
+
     function updateMap(selectedYear) {
+        currentYear = selectedYear
+
         d3.select("#year_label").text(selectedYear);
         const currentYearData = dataByYear[selectedYear] || {};
 
         const countries = g.selectAll('.country');
-
         countries.transition()
             .duration(200)
             .attr('fill', d => {
                 const val = currentYearData[d.id];
-                return val ? d3.interpolateBlues(val / 10) : "#cccccc";
+                return val ? colorScale(+val) : "#ffffff";
             });
 
         countries.select("title")
             .text(d => {
                 const name = idToName[d.id] || "Unknown Country";
-                return `${name}`;
+                const val = currentYearData[d.id] ? currentYearData[d.id].toFixed(2) : "No Data";
+                return `${name}\nScore: ${val}`;
             });
+        g.selectAll(".map-popup text").each(function() {
+            const popup = d3.select(this);
+            if (popup.text().includes("Score:")) {
+                const d = d3.select(this.parentNode).datum();
+                const newVal = currentYearData[d.id] ? currentYearData[d.id].toFixed(2) : "No Data";
+                popup.text(`Score: ${newVal}`);
+            }
+        });
     }
 
     d3.select("#year_slider").on("input", function() {
@@ -221,8 +317,39 @@ function renderWorldChart(){
 
 
 
-    //chloropleth Code
+    // Legend
 
+    const legend = svg_base.append("g")
+        .attr("transform", "translate(10, 370)");
+
+    const defs = svg_base.append("defs");
+
+    const linearGradient = defs.append("linearGradient")
+        .attr("id", "legend-gradient");
+
+    linearGradient.selectAll("stop")
+        .data(d3.range(0, 10, 0.1))
+        .enter()
+        .append("stop")
+        .attr("offset", d => `${d*100 }%`)
+        .attr("stop-color", d => colorScale(d*10));
+
+    legend.append("rect")
+        .attr("width", 200)
+        .attr("height", 10)
+        .style("fill", "url(#legend-gradient)");
+
+
+    const legendScale = d3.scaleLinear()
+        .domain([0, 10])
+        .range([0, 200]);
+
+    const legendAxis = d3.axisBottom(legendScale)
+        .ticks(6);
+
+    legend.append("g")
+        .attr("transform", "translate(0, 10)")
+        .call(legendAxis);
 
 }
 renderWorldChart();
